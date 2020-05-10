780c29333b8e12538af420fba6bb93152402673c
Saharsha Karki <sah.karki@gmail.com>
2020-05-10 23:25:19 +1000

    reordered rtree dependencies

--- config.sh
+++ config.sh
@@ -16,6 +16,8 @@ cd CloudComputingAssignment2/
 # add proxies in /etc/environment(internal)
 sudo cp environment /etc/environment
 
+# install libspatial index for python geopandas
+sudo apt-get install -y libspatialindex-dev
 
 # <for all nodes, including master>
 python3 -m venv .
@@ -25,13 +27,12 @@ pip install --upgrade pip
 pip install -r requirements.txt
 
 
-# install libspatial index for python geopandas
-sudo apt-get install -y libspatialindex-dev
+
 
 # enable the Apache CouchDB package repository
 sudo apt-get install -y apt-transport-https gnupg ca-certificates
 echo "deb https://apache.bintray.com/couchdb-deb bionic main"  | sudo tee -a /etc/apt/sources.list.d/couchdb.list
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 8756C4F765C9AC3CB6B85D62379CE192D401AB61
+sudo apt-key adv --keyserver keyserver.ubuntu.com:80 --recv-keys 8756C4F765C9AC3CB6B85D62379CE192D401AB61
 sudo apt update
 
 #install couched
