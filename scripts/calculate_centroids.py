8659602783a0069a6d75341b67e659659f2f54ac
Saharsha Karki <sah.karki@gmail.com>
2020-05-21 20:51:21 +1000

    updated script to generate json for centroids (but need geojson for markers)

--- scripts/calculate_centroids.py
+++ scripts/calculate_centroids.py
@@ -11,18 +11,23 @@ sadata.sindex
 
 # process sa2data centroids
 sa2data = []
+sa2d = {}
 for index, row in s.iterrows():
     try:
         #print(row['SA2_NAME16'], row['geometry'].centroid)
         c = row['geometry'].centroid
         d = {"name": row['SA2_NAME16'],
-            "coordinates" : {"lat": c.y, "long": c.x}}
+            "lat": c.y, "long": c.x}
         sa2data.append(d)
     except AttributeError:
         continue
 
-with open("../front-end/public/js/sa2centroids.js", "w") as f:
-    f.write("var data = " + str(sa2data))
+sa2d["data"] = sa2data
+
+
+with open("../front-end/public/js/sa2.json", "w") as f:
+    j = json.dumps(sa2d)
+    f.write(j)
 
 
 # process sa3data centroids
@@ -32,18 +37,19 @@ sadata = s[~s.geometry.isna()]
 sadata.sindex
 
 sa3data = []
-
+sa3d = {}
 for index, row in s.iterrows():
     try:
         #print(row['SA2_NAME16'], row['geometry'].centroid)
         c = row['geometry'].centroid
         d = {"name": row['SA3_NAME16'],
-            "coordinates" : {"lat": c.y, "long": c.x}}
+            "lat": c.y, "long": c.x}
         sa3data.append(d)
     except AttributeError:
         continue
+sa3d["data"] = sa3data
 
-with open("../front-end/public/js/sa3centroids.js", "w") as f:
-    f.write("var data = " + str(sa3data))
+with open("../front-end/public/js/sa3.json", "w") as f:
+    f.write(json.dumps(sa3d))
 
 print("your centroids are done!")
