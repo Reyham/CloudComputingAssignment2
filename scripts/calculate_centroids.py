a686563804e804ee7a0edf97509c90d1713edfde
Saharsha Karki <sah.karki@gmail.com>
2020-05-21 21:50:33 +1000

    script to produce centroids and icon for pins (Q1 implemented)

--- scripts/calculate_centroids.py
+++ scripts/calculate_centroids.py
@@ -1,6 +1,7 @@
 import geopandas, json
 import pandas as pd
 from shapely.geometry import Polygon
+from geojson import Feature, Point, FeatureCollection
 
 
 filename = "../shpfiles/SA2_2016_AUST.shp"
@@ -8,7 +9,50 @@ s = geopandas.read_file(filename)
 sadata = s[~s.geometry.isna()]
 sadata.sindex
 
+filename = "../shpfiles/SA3_2016_AUST.shp"
+sa3= geopandas.read_file(filename)
+sa3data = sa3[~sa3.geometry.isna()]
+sa3data.sindex
 
+
+def createGeoJson(data, type):
+    fcollection = []
+    if type == 2:
+        key = "SA2_NAME16"
+    elif type == 3:
+        key = "SA3_NAME16"
+    else:
+        return
+    for index, row in data.iterrows():
+        try:
+            #print(row['SA2_NAME16'], row['geometry'].centroid)
+            ctr = row['geometry'].centroid
+            name = row[key]
+            point = Point((ctr.x, ctr.y))
+            props = {"area":name}
+
+            feature = Feature(geometry=point, properties=props)
+            fcollection.append(feature)
+        except AttributeError:
+            continue
+    collection = FeatureCollection(fcollection)
+    frame = geopandas.GeoDataFrame.from_features(collection['features'], crs=sa3data.crs)
+    return frame.to_json()
+
+sa2c = createGeoJson(sadata, 2)
+sa3c = createGeoJson(sa3data, 3)
+
+with open("../front-end/public/js/sa2.geojson", "w") as f:
+    f.write(sa2c)
+
+
+with open("../front-end/public/js/sa3.geojson", "w") as f:
+    f.write(sa3c)
+
+print("your centroids are done!")
+
+
+'''
 # process sa2data centroids
 sa2data = []
 sa2d = {}
@@ -30,6 +74,10 @@ with open("../front-end/public/js/sa2.json", "w") as f:
     f.write(j)
 
 
+
+
+
+
 # process sa3data centroids
 filename = "../shpfiles/SA3_2016_AUST.shp"
 s = geopandas.read_file(filename)
@@ -38,6 +86,7 @@ sadata.sindex
 
 sa3data = []
 sa3d = {}
+
 for index, row in s.iterrows():
     try:
         #print(row['SA2_NAME16'], row['geometry'].centroid)
@@ -53,3 +102,4 @@ with open("../front-end/public/js/sa3.json", "w") as f:
     f.write(json.dumps(sa3d))
 
 print("your centroids are done!")
+'''
