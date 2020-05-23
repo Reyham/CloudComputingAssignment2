7dc507eb93ff17a2afb3721869a9da3a328cd154
Saharsha Karki <sah.karki@gmail.com>
2020-05-23 21:34:05 +1000

    added aurin data to map markers

--- front-end/utils/couch_routes.js
+++ front-end/utils/couch_routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const nano = require('nano')('http://admin:admin@localhost:5984');
-const store = nano.db.use('database');
+const nano = require('nano')('http://admin:1234@localhost:5984');
+const store = nano.db.use('db6');
 
 get_view = async function(view_name) {
 
