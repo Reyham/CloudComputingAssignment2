0652163e9833b782dcadbb6bbe1481a48513ab6e
HemanthChalla <63779423+HemanthChalla@users.noreply.github.com>
2020-05-26 14:58:18 +1000

    Final Update

--- automation-script/run-nectar.sh
+++ automation-script/run-nectar.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+############################################################
+# # Cluster and Cloud Computing (COMP90024) Assignment 2 # #
+# # The University of Melbourne                          # #
+# # Group Number: 48                                     # #
+# # Authors:                                             # #
+# # 1. Hemanth Pavan Kumar Challa - 1064637              # #
+# # 2. Sai Varun Reddy Ankireddy  - 1152791              # #
+# # 3. Abhisha Nirmalathas        - 913405               # # 
+# # 4. Saharsha Karki             - 1042219              # #
+# # 5. Reyham Noorjono            - 1018055              # #
+############################################################
+
 . ./unimelb-comp90024-2020-grp-48-openrc.sh; ansible-playbook --ask-become-pass playbook.yaml
